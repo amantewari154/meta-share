a6ef57b8e0e28dbfa2aecd45beeb9671f7c1ee7b
Aman Tewari <amantewari154@gmail.com>
2022-06-08 09:39:46 +0530

    initial

---
+++ config/db.js
@@ -0,0 +1,14 @@
+require('dotenv').config();
+const mongoose = require('mongoose');
+function connectDB() {
+    // Database connection 🥳
+    mongoose.connect(process.env.MONGO_CONNECTION_URL);
+    const connection = mongoose.connection;
+    connection.once('open', () => {
+        console.log('Database connected.');
+    }).on('error', function (err) {
+        console.log(err);
+      });
+}
+
+module.exports=connectDB;
